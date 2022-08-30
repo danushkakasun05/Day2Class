7769be82f030a4f92e7e34f2761126d1acccb480
danushka kasun <danushkakasun05>
2022-08-30 22:49:21 +0530

    squarecal

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        SquareCalculator sqcal = new SquareCalculator();
+        sqcal.setHeight(18);
+        sqcal.setWidth(14);
+        System.out.println(sqcal.getHeight()*sqcal.getWidth());
+
+    }
+}
---
+++ src/SquareCalculator.java
@@ -0,0 +1,22 @@
+public class SquareCalculator {
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    private double height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    private double width;
+
+}
