8141b1b51acffbb63077bd0db0fbbf5655ffd70d
danushka kasun <danushkakasun05>
2022-08-31 11:19:24 +0530

    31/08/2022 11.19

---
+++ src/testing/Maintest.java
@@ -0,0 +1,9 @@
+import com.area.Rectangle;
+
+public class Maintest {
+    public static void main(String[] args) {
+        Rectangle rec =new Rectangle(12,12);
+        System.out.println("Area: "+rec.areaCal());
+
+    }
+}
